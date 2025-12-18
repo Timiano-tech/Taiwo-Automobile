29790323119b347e5ccc2264d27fc587cd229297
Timiano <timiajayi729@gmail.com>
2025-12-18 20:13:28 +0100

    Worked on the main Page, in Header.jsx

--- src/routes/Header.jsx
+++ src/routes/Header.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
-import '../Styles/Header.css'
+import '../Styles/Header.css';
+import Features from '../routes/Features.jsx';
+import Services from '../routes/Services.jsx';
+import Testimonial from '../routes/Testimonial.jsx';
 
 const Header = () => {
   return (
     <div>
       <section className="hero" id="home">
-<div className="header__text">
+      <div className="header__text">
         <h2>Welcome to<span className="t-autos"> T-Autos</span></h2>
         <p>Skilled and experienced automotive mechanics with passion for keeping your car running smoothly.</p>
         <a href="#message_container">Get in touch</a>
     </div>
 
     </section>
+    <Features/>
+    <Services/>
+    <Testimonial/>
     </div>
   )
 }
